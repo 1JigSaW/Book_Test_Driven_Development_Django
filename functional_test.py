a22739fb72db4aaa0357e6d027db97d7dc898462
JigSaW <jigsaw11211@gmail.com>
2021-03-15 16:20:21 +0300

    ...

--- functional_test.py
+++ functional_test.py
@@ -4,39 +4,55 @@ import unittest
 import time
 
 class NewVisitorTest(unittest.TestCase):
-	'''test new visitor'''
-	def setUp(self):
-		'''install'''
-		self.browser = webdriver.Firefox()
+    '''test new visitor'''
+    def setUp(self):
+        '''install'''
+        self.browser = webdriver.Firefox()
 
-	def TearDown(self):
-		'''dismantling'''
-		self.browser.quit()
+    def TearDown(self):
+        '''dismantling'''
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+    def check_for_row_in_list_table(self, row_text):
+        '''The confirmation row in list table'''
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Make a peacock feather fly')
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		# self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """docstring for test_can_start_a_list_and_retrieve_it_later"""
+        self.browser.get('http://localhost:8000')
 
-		# inputbox.send_keys('Buy peacock feathers')
-		# inputbox.send_keys(Keys.ENTER)
-		# time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feather', [row.text for row in rows])
-		self.assertIn(
-			'2: Make a peacock feather fly',
-			[row.text for row in rows]
-		)
-		self.fail('Закончить тест!')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+
+
+        inputbox.send_keys(Keys.ENTER)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+ #       self.assertTrue(
+ #           any(row.text == '1: Buy peackock feathers' for row in rows)#,
+ #           "New to-do item did not appear in table -- it was %s" %\
+ #               (table.text,),
+ #       )
+
+        self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
