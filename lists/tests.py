c34b7353769cde9019e3c3c509060cadf668ce87
JigSaW <jigsaw11211@gmail.com>
2021-03-15 13:01:21 +0300

    update tets and fix errors

--- lists/tests.py
+++ lists/tests.py
@@ -2,20 +2,28 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
-	'''home page test '''
 
-	def test_root_url_resolves_to_home_page_view(self):
-		'''root url converted to representation home page'''
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  
+        self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_correct_html(self):
-		'''home page return true html'''
-		request = HttpRequest()
-		response = home_page(request)
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = home_page(request)  
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>To-Do lists</title>', html)  
+        self.assertTrue(html.strip().endswith('</html>'))
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')  
+
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html') 
