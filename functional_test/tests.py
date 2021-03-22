e3708b818eea0520934e98b319f70478d593e9f0
JigSaW <misho_1998@mail.ru>
2021-03-22 18:08:56 +0300

    maket

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,157 +1,146 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-from selenium import webdriver # нужно всегда следить за обновлениями selenium (pip install --upgrade selenium)
-                               # и, соотвественно, драйвером geckodriver
-from selenium.webdriver.common.keys import Keys
-import time
-from django.test import LiveServerTestCase #import unittest
 from selenium.common.exceptions import WebDriverException
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import time
+import os
 
-MAX_WAIT = 10 # максимальным количеством времени, которое готовы ожидать. 
-              # 10 секунд более чем достаточно для отлавливания любых незначительных сбоев или
-              # случайных замедлений
+MAX_WAIT = 10
 
-
-class NewVisitorTest(LiveServerTestCase):
-    '''Тест нового посетителя'''
-
-    def setUp(self):
-        '''Установка'''
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        '''Размонтирование'''
-        self.browser.quit()
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def wait_for_row_in_list_table(self, row_text):
-        '''ожидать строку в таблице списка'''
         start_time = time.time()
         while True:
             try:
-                tag_id = 'id_list_table'
-                table = self.browser.find_element_by_id(tag_id)
+                table = self.browser.find_element_by_id('id_list_table')
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
-            except(AssertionError, WebDriverException) as e: # отлавливаем два типа исключений:
-                                                             # 1. WebDriverException для случая, когдастраница не загрузилась и
-                                                             # Selenium не может найти табличный элемент на странице,
-                                                             # 2. AssertionError – для случая, когда таблица имеется,
-                                                             # но это, возможно, таблица до перезагрузок страницы,
-                                                             # поэтому в ней пока нет нашей строки
-                if time.time() - start_time > MAX_WAIT: # 6
-                    raise e     # поднимаем исключение и даем ему «всплыть» в тесте при превышении лимита времени
-                time.sleep(0.5) # если мы отлавливаем исключение, то ожидаем в течение короткого периода
-                                # и в цикле делаем повторную попытку
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_layout_and_styling(self):
-        '''тест макета и стилевого оформления'''
-        # Лена открывает домашнюю страницу
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_for_one_user(self):
+        # Edit has heard about a cool new online to-do app. She goes 
+        # to check out its homepage
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768) # установка фиксированных значений окна
 
-        # Она замечает, что поле ввода аккуратно центрировано
-        inputbox = self.browser.find_element_by_id('id_new_item') #отыскиваем элемент input
-        self.assertAlmostEqual( # assertAlmostEqual помогает нам справиться с погрешностями округления и случайными странностями из-за полос прокрутки и т.п.
-            inputbox.location['x'] + inputbox.size['width'] / 2, # смотрим на его размер и расположение и проверяем: расположен ли он посередине страницы
-            512,
-            delta=10 # погрешность +/-10 пикселов
-        )
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text 
+        self.assertIn('To-Do', header_text)
 
-        # Она начинает новый список и видит, что поле ввода там тоже аккуратно центрировано
-        inputbox.send_keys('тестирование')
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fiching lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: тестирование')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        
+        # Edith wonders wether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that efect
+        # self.fail('Finish the test!')
+        # She visits that URL -her to-do list is still there
+        
+        # The page updates again, and now shows both items on her list
+
+
+        # Satisfied, she goes back to sleep
+
+    def test_multiple_users_can_start_lists_al_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now  a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is comming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        
+        # Francis visits the home page, There is  no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting thean Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        
+        # Francis gets his own unique URLD
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, thereis no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfed, they both go back to sleep
+         
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
+            512, 
+            delta=10            
+        )
+
+        # She starts a new list and  sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
             512,
             delta=10
         )
-
-    def test_can_start_a_list_for_one_user(self):
-        '''тест: можно начать список для одного пользователя'''
-        # Лена слышала про крутое новое онлайн-приложение со списком
-        # неотложных дел. Она решает оценить его домашнюю страницу
-        self.browser.get(self.live_server_url)
-
-        # Она видит, что заголовок и шапка страницы говорят о списках
-        # неотложных дел
-        self.assertIn('To-Do', self.browser.title)
-
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Начать новый список неотложных дел', header_text)
-
-        # Ей сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Введите элемент списка'
-        )
-
-        # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
-        # вязание рыболовных мушек)
-        inputbox.send_keys('Купить павлиньи перья')
-
-        # Когда она нажимает enter, страница обновляется, и теперь страница
-        # содержит "1: Купить павлиньи перья" в качестве элемента списка
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
-
-        # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
-        # Она вводит "Сделать мушку из павлиньих перьев" (Лена очень методична)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Сделать мушку из павлиньих перьев')
-        inputbox.send_keys(Keys.ENTER)
-
-        # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')        
-        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
-
-    def test_multiple_users_can_start_lists_at_different_urls(self):
-        '''тест: многочисленные пользователи могут начать списки по разным URL'''
-
-        # Лена начинает новый список
-        self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Купить павлиньи перья')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
-
-        # Лена замечает, что ее список имеет уникальный URL-адрес
-        helen_list_url = self.browser.current_url
-        self.assertRegex(helen_list_url, '/lists/.+') # assertRegex – вспомогательная функция из unittest, которая проверяет, 
-                                                      # соответствует ли строка регулярному выражению; 
-                                                      # используется, чтобы проверить реализацию новой RESTʼовской конструкции.
-
-        # Теперь новый пользователь, Эльдар, приходит на сайт.
-        ## Мы используем новый сеанс браузера, тем самым обеспечивая, чтобы никакая
-        ## информация от Эдит не прошла через данные cookie и пр.
-        self.browser.quit()
-        self.browser = webdriver.Firefox()
-
-        # Эльдар посещает домашнюю страницу. Нет никаких признаков списка Лены
-        self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Купить павлиньи перья', page_text)
-        self.assertNotIn('Сделать мушку', page_text)
-
-        # Эльдар начинает новый список, вводя новый элемент
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Купить молоко')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Купить молоко')
-
-        # Эльдар получает уникальный URL-адрес
-        eldar_list_url = self.browser.current_url
-        self.assertRegex(eldar_list_url, '/lists/.+')
-        self.assertNotEqual(eldar_list_url, helen_list_url)
-
-        # Опять-таки, нет ни следа от списка Лены
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Купить павлиньи перья', page_text)
-        self.assertIn('Купить молоко', page_text)
-
-
-        # Удовлетворенные, они оба ложатся спать
-        self.browser.quit()
