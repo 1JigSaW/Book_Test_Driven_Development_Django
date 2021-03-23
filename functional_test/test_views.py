6d781f6e06808349cbbaa89b30e125d5ea42564d
JigSaW <misho_1998@mail.ru>
2021-03-23 09:56:13 +0300

    new tests files

---
+++ functional_test/test_views.py
@@ -0,0 +1,142 @@
+from django.utils.html import escape
+
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.models import Item, List
+from django.test import TestCase
+
+
+class HomePageTest(TestCase):
+    """тест домашней страницы"""
+
+    def test_uses_home_template(self):
+        """тест: домашняя страница возвращает правильный html"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_home_page_uses_item_form(self):
+        """тест: домашняя страница использует форму для элемента"""
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+
+class ListViewTest(TestCase):
+    """тест представления списка"""
+
+    def test_uses_list_template(self):
+        """тест: используется шаблон списка"""
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertTemplateUsed(response, 'lists/list.html')
+
+    def test_passes_correct_list_to_template(self):
+        """тест: передаётся правильный шаблон списка"""
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
+    def test_displays_only_items_for_that_list(self):
+        """тест: отображаются элементы только для этого списка"""
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='другой элемент списка 1', list=other_list)
+        Item.objects.create(text='другой элемент списка 2', list=other_list)
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'другой элемент списка 1')
+        self.assertNotContains(response, 'другой элемент списка 2')
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+        self.client.post('/lists/new', data={'text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirect_after_POST(self):
+        """тест: переадресует после post-запроса"""
+        response = self.client.post('/lists/new', data={'text': 'A new list item'})
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_for_invalid_input_renders_home_template(self):
+        """тест на допустимый ввод: отображает домашний шаблон"""
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        """тест: ошибки валидации выводятся на домашней странице"""
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        """тест на недопустимый ввод: форма передается в шаблон"""
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_invalid_list_items_arent_saved(self):
+        """тест: сохраняются недопустимые элементы списка"""
+        self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        """тест: можно сохранить post-запрос в существующий список"""
+        other_lisr = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_POST_redirects_to_list_vie(self):
+        """тест: переадресация в представление списка"""
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def post_invalid_input(self):
+        """отправляет недопустимый ввод"""
+        list_ = List.objects.create()
+        return self.client.post(f'/lists/{list_.id}/', data={'text': ''})
+
+    def test_for_invalid_input_renders_list_template(self):
+        """тест на недопустимый ввод: отображается шаблон списка"""
+        response = self.post_invalid_input()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lists/list.html')
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        """тест на недопустимый ввод: форма передается в шаблон"""
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_for_invalid_input_shows_error_on_page(self):
+        """тест на недопустимый ввод: на странице показывается ошибка"""
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_displays_item_form(self):
+        """тест отображения формы для элемента"""
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, 'name="text"')
---
+++ lists/tests/test_models.py
@@ -0,0 +1,72 @@
+from django.core.exceptions import ValidationError
+
+from lists.models import Item, List
+from django.test import TestCase
+
+
+class ItemModelTest(TestCase):
+    """тест модели элемента списка"""
+
+    def test_default_text(self):
+        """тест заданного по умолчанию текста"""
+        item = Item()
+        self.assertEqual(item.text, '')
+
+    def test_item_is_related_to_list(self):
+        """тест: элемент связан со списком"""
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
+
+    def test_cannot_save_empty_list_items(self):
+        """тест: нельзя добавить пустые элементы списка"""
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
+        # Альтернатива with
+        # try:
+        #     item.save()
+        #     self.fail('Метод save должен поднять исключение')
+        # except ValidationError:
+        #     pass
+
+
+class ListModelTest(TestCase):
+
+    def test_get_absolute_url(self):
+        """тест: получен абсолютный url"""
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
+    def test_duplicate_items_are_invalid(self):
+        """тест: повторы элементов не допустимы"""
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item.objects.create(list=list_, text='bla')
+            item.full_clean()
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        """тест: МОЖЕТ сохранить тот же элемент в разные списки"""
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean()  # не должен поднять исключение
+
+    def test_list_ordering(self):
+        """тест упорядочения списка"""
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='i1')
+        item2 = Item.objects.create(list=list1, text='item 2')
+        item3 = Item.objects.create(list=list1, text='3')
+        self.assertEqual(list(Item.objects.all()), [item1, item2, item3])
+
+    def test_string_representation(self):
+        """тест строкового представления"""
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
