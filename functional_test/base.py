d45d51a1123cb64fc731ac14604371499f2804bf
JigSaW <misho_1998@mail.ru>
2021-03-24 13:29:03 +0300

    new'

--- functional_test/base.py
+++ functional_test/base.py
@@ -1,51 +1,62 @@
-  
-from selenium.common.exceptions import WebDriverException
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-import time
-import os
-
-MAX_WHITE = 10
+from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import unittest
+import sys
 
 
 class FunctionalTest(StaticLiveServerTestCase):
-    """функциональный тест"""
-    def setUp(self):
-        """установка"""
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'https://' + staging_server
-
-    def tearDown(self):
-        """демонтаж"""
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        """ожидать строку в таблице списка"""
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                cls.live_server_url = ''
+                cls.against_staging = True
                 return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WHITE:
-                    raise e
-                time.sleep(0.5)
+        super().setUpClass()
+        cls.against_staging = False
+        cls.server_url = cls.live_server_url
 
-    def wait_for(self, fn):
-        """ожидать"""
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WHITE:
-                    raise e
-                time.sleep(0.5)
+    @classmethod
+    def tearDownClass(cls):
+        # if cls.server_url == cls.live_server_url:
+        #     super().tearDownClass()
+        if not cls.against_staging:
+            super().tearDownClass()
+
+    def setUp(self):
+        if self.against_staging:
+            reset_database(self.server_host)
+        self.browser = webdriver.Firefox(executable_path=r'e:\workspace\tdd_python\geckodriver.exe')
+        self.browser.implicitly_wait(3)
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def get_item_input_box(self):
-        """получить поле ввода для элемента"""
         return self.browser.find_element_by_id('id_text')
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=3).until(
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was {}'.format(element_id, self.browser.find_element_by_tag_name('body').text)
+        )
+        
+    def wait_to_be_logged_in(self, email):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
+
+
+    def tearDown(self):
+        self.browser.quit()
--- functional_test/test_list_item_validation.py
+++ functional_test/test_list_item_validation.py
@@ -1,63 +1,70 @@
-from functional_test.base import FunctionalTest
+from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
+from unittest import skip
+import time
 
 
 class ItemValidationTest(FunctionalTest):
-    """тест валидации элемента списка"""
 
-    def test_cannot_add_empty_list_item(self):
-        """тест: нельзя добавлять пустые элементы списка"""
-        # Эдит открывает домашнюю страницу и случайно пытается отправить
-        # пустой элемент списка. Она нажимает Enter на пустом поле ввода
-        self.browser.get(self.live_server_url)
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
+    def test_cannot_add_empty_list_items(self):
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
         self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # Браузер перехватывает запрос и не перезагружает страницу со списком
-        self.wait_for(lambda: self.browser.find_element_by_css_selector(
-            '#id_text:invalid'
-        ))
+        error = self.get_error_element()
+        self.assertEqual(error.text, "You can't have an empty list item" )
 
-        # Она пробует снова, теперь с неким текстом для элемента, и теперь
-        # это срабатывает
-        self.get_item_input_box().send_keys('Buy milk')
-        self.wait_for(lambda: self.browser.find_element_by_css_selector(
-            '#id_text:valid'
-        ))
+
+        self.get_item_input_box().send_keys('Buy milk\n')
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
 
-        # Как ни странно Эдит решает отправить второй пустой элемент списка
+        self.get_item_input_box().send_keys('\n')
         self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # И снова браузер не подчинится
-        self.wait_for_row_in_list_table('1: Buy milk')
-        self.wait_for(lambda: self.browser.find_element_by_css_selector(
-            '#id_text:invalid'
-        ))
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.get_error_element()
+        self.assertEqual(error.text, "You can't have an empty list item" )
 
-        # И она может его исправить, заполнив поле неким текстом
-        self.get_item_input_box().send_keys('Make tea')
-        self.wait_for(lambda: self.browser.find_element_by_css_selector(
-            '#id_text:valid'
-        ))
+        self.get_item_input_box().send_keys('Make tea\n')
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
-        self.wait_for_row_in_list_table('2: Make tea')
-
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
+        
+        
     def test_cannot_add_duplicate_items(self):
-        """тест: нельзя добавить повторяющиеся элементы"""
-        # Эдит открывает домашнюю страницу и начинает новый список
-        self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Buy wellies')
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy wellies\n')
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy wellies')
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy wellies')
 
-        # Она случайно пытается ввести повторяющийся элемент
-        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys('Buy wellies\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.get_error_element()
+        self.assertEqual(error.text, "You've already got this in your list" )
+
+    def test_error_messages_are_cleared_on_input(self):
+
+        self.browser.get(self.server_url)
+        # self.get_item_input_box().send_keys('\n')
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # Она видит полезное сообщение об ошибке
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "You've already got this in your list"
-        ))
+
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        self.get_item_input_box().send_keys('a')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
